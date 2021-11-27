2daafa6326aca30f973a79c97f83d00e4723f423
deepti <75380267+deepti-3101@users.noreply.github.com>
2021-11-27 14:38:12 +0530

    Update PoC.py

--- PoC.py
+++ PoC.py
@@ -7,14 +7,14 @@ def sim(similarity, hash1, hash2):
     threshold = 1 - similarity / 100
     diff_limit = int(threshold * (8 ** 2))
     if np.count_nonzero(hash1 != hash2) <= diff_limit:
-        print("Images Similar with accuracy upto : " + str(similarity) + "%")
+        return True
     else:
-        print("Images not Similar with the given accuracy: " + str(similarity) + "%")
+        return False
 
-
+#---------------------------------------------------------#
 original = input("Enter the Original Image : ")
 check = input("Enter the image to be checked : ")
-
+#-------------------------------------------------------#
 with Image.open(original) as imgOri:
     hashOri = imagehash.average_hash(imgOri, 8).hash
     print(hashOri)
@@ -25,4 +25,4 @@ with Image.open(check) as imgCK:
 
 print("\n\n=-=-=-=-=-=-=-=-==-=-=-=-=-===\n\n")
 
-sim(80, hashOri, hashCK)
+sim(82, hashOri, hashCK)
