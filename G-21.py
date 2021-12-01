d3e9970c1b82ad305b35ae9ae19bc51540b55c93
Dominic Walter <59534138+DominicwalterLOF@users.noreply.github.com>
2021-12-01 19:01:05 +0530

    Update G-21.py

--- G-21.py
+++ G-21.py
@@ -17,7 +17,7 @@ from pathlib import Path
 Path("D:/Genesis-21/Searches").mkdir(parents=True, exist_ok=True)
 
 username = "dem.odummy"
-password = "orproject5"  # Be careful, don't accidentally expose your password when committing
+password = "***********"  # Be careful, don't accidentally expose your password when committing
 name = "D:/Genesis-21/Searches"
 target = "C:\\tmp\\1.jpg"
 main = OrderedDict()
