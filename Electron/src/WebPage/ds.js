d55538f5eb8d2b9b25a65e82cba9ab8e8b000bc8
deepti <75380267+deepti-3101@users.noreply.github.com>
2021-12-22 14:18:57 +0530

    Add files via upload

---
+++ Electron/src/WebPage/ds.js
@@ -0,0 +1,47 @@
+const counter = document.querySelector(".counter");
+let count = 0;
+setInterval(() => {
+ if (count == 92) {
+  clearInterval(count);
+ } else {
+  count += 1;
+  counter.textContent = count + "%";
+ }
+}, 42);
+
+
+
+
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.getElementById("hour").innerHTML=today;
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
---
+++ Electron/src/WebPage/firebase.js
@@ -0,0 +1,14 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBxvxqKX_LXsWBaA9BZD4k6ugtZ8kuHOYE",
+    authDomain: "login-with-firebase-41bc0.firebaseapp.com",
+    projectId: "login-with-firebase-41bc0",
+    storageBucket: "login-with-firebase-41bc0.appspot.com",
+    messagingSenderId: "362050156322",
+    appId: "1:362050156322:web:de1bb47ee8b7486316977b",
+    measurementId: "G-KVKQJKX4FL"
+  };
+
+
+const app = initializeApp(firebaseConfig);
